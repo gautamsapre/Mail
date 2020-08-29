77866b59e71353a09c782c53467115fefdfcb55b
gautamsapre <44683449+gautamsapre@users.noreply.github.com>
2020-08-28 20:23:01 -0500

    Fixed Static Change in settings

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -15,7 +15,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
+ 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
